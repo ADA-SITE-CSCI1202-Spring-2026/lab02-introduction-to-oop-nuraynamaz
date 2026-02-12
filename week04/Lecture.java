a90f94e46c45895a5334fa279970266f7ccd0bbf
Nuray Namazova <nnamazova20412@ada.edu.az>
2026-02-12 10:48:55 +0400

    updated

---
+++ week04/Lecture.java
@@ -0,0 +1,11 @@
+import geometry.Segment;
+
+public class Lecture {
+    public static void main(String[] args)
+    {
+        Segment s = new Segment(3.14f, 1.1f, 2.0f, 0.3f);
+        System.out.println("x1 = " + s.getP1().getX());
+    }
+
+
+}
---
+++ week04/geometry/Point.java
@@ -0,0 +1,33 @@
+package geometry;
+
+public class Point {
+    private float x;
+    private float y;
+
+    public Point()
+    {
+        x = 0f;
+        y = 0f;
+    }
+    public Point(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point (Point p)
+    {
+        this.x = p.x;
+        this.y = p.y;
+    }
+
+    public float getX()
+    {
+        return this.x;
+    }
+
+    public void setX(float x)
+    {
+        this.x = x;
+    }
+}
